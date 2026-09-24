71e5de567132a8c66884fa35c64af6b3fe46dab7
Mark Battistella <mark_battistella+github@hotmail.com>
2026-09-25 07:28:30 +1000

    Release 1.0.1

--- Sources/PublishDev/ReleaseVersion.swift
+++ Sources/PublishDev/ReleaseVersion.swift
@@ -9,7 +9,7 @@ import Foundation
 /// Stable release versions only. Drafts, prereleases, and non-version tags are not updates.
 struct ReleaseVersion: Comparable, Sendable, CustomStringConvertible {
     // Use make release VERSION=x.y.z to commit the version and publish a validated release.
-    static let current = ReleaseVersion(major: 1, minor: 0, patch: 0)
+    static let current = ReleaseVersion(major: 1, minor: 0, patch: 1)
 
     let major: Int
     let minor: Int
